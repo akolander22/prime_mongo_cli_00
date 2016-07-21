11a8443bdbbb1f3836d086056c30632c4307a56e
Andrew Kolander <akolander22@gmail.com>
2016-07-21 14:33:43 -0500

    new commit

--- queries.js
+++ queries.js
@@ -6,8 +6,6 @@
 
 4.> db.bios.find({"awards.award": {$in: ["Turing Award"]}}).pretty()
 
-
-//will come back to this one
 > db.bios.find({_id: {$gte:3,$lte:7}}).pretty()
 
 6.> db.bios.find({"awards.year" : {$lt : 2000}}).pretty()
